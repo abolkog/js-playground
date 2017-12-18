8a0738f93266bec8f27da1f9b16fee1c6521ac9a
Khalid Elshafie <abolkog@gmail.com>
2017-12-18 22:59:45 +0900

    Adding redux globally

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React  from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import _ from 'lodash';
 
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App';
 import reducers from './reducers';
+
+_.extend(window, { _, Redux: { createStore, combineReducers } });
 
 
 ReactDOM.render(
