8bf9653b034a9ec7f460bb0dff41bb0b2ee9fc0c
Khalid Elshafie <abolkog@gmail.com>
2018-12-21 15:20:12 +0900

    Remove eval

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,34 @@
+import vm from "vm";
+import { createStore } from "redux";
 import { transform } from "babel-standalone";
 import { CODE_SUCCESS, CODE_ERROR, TOGGLE_MODAL } from "./types";
 
 export const handleCode = code => {
-  try {
-    const transformed = transform(code, { presets: ["stage-0"] }).code;
-    const result = eval(transformed);
-    return { type: CODE_SUCCESS, payload: result };
-  } catch (e) {
-    return { type: CODE_ERROR, payload: e.toString() };
-  }
+  return async dispatch => {
+    try {
+      const transformed = transform(code, { presets: ["stage-0"] }).code;
+      // const result = eval(transformed);
+      const result = await runCodeInVM(transformed);
+      dispatch({ type: CODE_SUCCESS, payload: result });
+    } catch (e) {
+      dispatch({ type: CODE_ERROR, payload: e.toString() });
+    }
+  };
+};
+
+const runCodeInVM = code => {
+  return new Promise((resolve, reject) => {
+    try {
+      const result = vm.runInNewContext(code, {
+        console: console,
+        setTimeout: setTimeout,
+        Redux: { createStore }
+      });
+      resolve(result);
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 export const toggleModal = () => ({ type: TOGGLE_MODAL });
