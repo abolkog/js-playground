d55074fb1a50b23cbfa63fcd14e4563bc5f79c1d
Khalid Elshafie <abolkog@gmail.com>
2018-12-21 15:21:57 +0900

    Cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-import _ from "lodash";
-import registerServiceWorker from "./registerServiceWorker";
 
+import registerServiceWorker from "./registerServiceWorker";
 import App from "./components/App";
 import store from "./store";
 import { CONSOLE_LOG } from "./actions/types";
-
-_.extend(window, { _, Redux: { createStore, combineReducers } });
 
 const consoleProxy = console.log;
 
