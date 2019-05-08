d4185efd794adb53922cb775b98771cd675848f9
Khalid Elshafie <abolkog@gmail.com>
2019-05-08 22:43:27 +0900

    Minor bug fix

--- src/reducers/CodeReducer.js
+++ src/reducers/CodeReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
   CODE_RUN_ERROR,
   CODE_RUN_SUCCESS,
@@ -27,7 +28,13 @@ export default (state = INITIAL_STATE, action) => {
       };
     case CODE_RUN_SUCCESS:
       const { result } = state;
-      if (action.payload) result.push(action.payload);
+      if (action.payload) {
+        if (!_.isString(action.payload)) {
+          result.push(JSON.stringify(action.payload));
+        } else {
+          result.push(action.payload);
+        }
+      }
       return { ...state, result, error: '', loading: false };
     case CODE_RUN_ERROR:
       return { ...state, error: action.payload, result: [], loading: false };
