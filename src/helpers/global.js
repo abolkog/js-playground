e7caa9f8e2009843d806fc0d5a95b5ef84c71ab6
Khalid Elshafie <abolkog@gmail.com>
2022-07-19 17:00:57 +1000

    Remove momentjs

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -1,8 +1,7 @@
 import _ from 'lodash';
 import { createStore, combineReducers } from 'redux';
 import axios from 'axios';
-import moment from 'moment-timezone';
 import * as luxon from 'luxon';
 import faker from 'faker';
 
-_.extend(window, { _, axios, moment, luxon, faker, Redux: { createStore, combineReducers } });
+_.extend(window, { _, axios, luxon, faker, Redux: { createStore, combineReducers } });
